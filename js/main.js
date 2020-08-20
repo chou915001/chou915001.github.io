efe36421be65b1cc4c077c130b18d5df5d2f7b21
chou915001 <69897185+chou915001@users.noreply.github.com>
2020-08-21 01:12:20 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -353,7 +353,7 @@ function createChart() {
 
         plotOptions: {
             series: {
-                compare: 'percent',
+                compare: 'value',
                 showInNavigator: true
 			},
 			candlestick: {
